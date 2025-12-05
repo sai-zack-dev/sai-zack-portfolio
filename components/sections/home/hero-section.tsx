0d17183386e98519134585ee237e765b859358ff
sai-zack-dev <saizlinh@gmail.com>
2025-12-05 15:12:47 +0800

    tech stack section

--- components/sections/home/hero-section.tsx
+++ components/sections/home/hero-section.tsx
@@ -1,20 +1,42 @@
+import { AuroraText } from "@/components/ui/aurora-text";
+import { PixelatedCanvas } from "@/components/ui/pixelated-canvas";
+
 export default function HeroSection() {
   return (
     <div className="flex min-h-screen w-full max-w-4xl items-center justify-center bg-white dark:bg-black mx-auto py-24 h-screen flex-col sm:flex-row">
-      <div className="w-full h-full justify-center items-center flex">
-        <div className="avatar mb-4">
-          <div className="w-50 h-50 rounded-full bg-gray-500"></div>
-        </div>
+      <div className="w-full h-full justify-center items-center flex p-5">
+          <PixelatedCanvas
+            src="/frame.png"
+            width={400}
+            height={400}
+            cellSize={3}
+            dotScale={0.9}
+            shape="square"
+            backgroundColor="#000000"
+            dropoutStrength={0.4}
+            interactive
+            distortionStrength={3}
+            distortionRadius={80}
+            distortionMode="swirl"
+            followSpeed={0.2}
+            jitterStrength={4}
+            jitterSpeed={4}
+            sampleAverage
+            tintColor="#FFFFFF"
+            tintStrength={0.2}
+            className="rounded-xl border border-neutral-800 shadow-lg"
+          />
       </div>
       <div className="w-full h-full justify-center items-start flex flex-col p-5 gap-3">
-        <p> Hello, It's me.</p>
-        <h1 className="text-5xl">SAI ZACK</h1>
-        <h3 className="text-2xl">Software Engineer / Designer / Developer</h3>
-        <p>
+        <p className="text-sm"> Hello, It's me.</p>
+        <AuroraText className="text-6xl font-bold">SAI ZACK</AuroraText>
+        <h3 className="text-2xl">Software Engineer</h3>
+        <p className="text-sm">
           I build web, mobile, and desktop solutions in JavaScript and PHP,
-          along with supporting UI/UX work. I deliver complete,
-          production-ready applications from idea to launch.
+          along with supporting UI/UX work. I deliver complete, production-ready
+          applications from idea to launch.
         </p>
+        <button>Download Resume</button>
       </div>
     </div>
   );
--- components/sections/home/tech-stack.tsx
+++ components/sections/home/tech-stack.tsx
@@ -1,14 +1,143 @@
+"use client";
+import {
+  AnimatedSpan,
+  Terminal,
+  TypingAnimation,
+} from "@/components/ui/terminal";
+import { File, Folder, Tree } from "@/components/ui/file-tree";
+import {
+  SiPhp,
+  SiNestjs,
+  SiReact,
+  SiTailwindcss,
+  SiNextdotjs,
+  SiNuxtdotjs,
+  SiFlutter,
+  SiElectron,
+  SiGit,
+  SiDocker,
+  SiFigma,
+} from "react-icons/si";
+import { FcLinux } from "react-icons/fc";
+import { cn } from "@/lib/utils";
+import { InteractiveGridPattern } from "@/components/ui/interactive-grid-pattern";
+
+
 export default function TechStack() {
   return (
-    <main className="flex min-h-screen w-full max-w-4xl items-center justify-center bg-white dark:bg-black sm:items-start mx-auto py-24 h-screen">
-      <div className="w-full h-full bg-green-100 justify-center flex flex-col items-center">
-        <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Tech Stack Section
-        </h1>
-        <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-          This is the Tech Stack Section.
-        </p>
+    <main className="flex min-h-screen w-full max-w-4xl items-center justify-center bg-white dark:bg-black sm:items-start mx-auto py-24">
+      <div className="w-full h-full p-5">
+        <h1 className="text-2xl">Technologies & Tools</h1>
+        <div className="grid md:grid-cols-2  w-full gap-5 py-5">
+          <div className="bg-violet-100 rounded-2xl row-span-2">
+              <Tree
+                className="overflow-hidden bg-background p-2 z-0 h-full w-full rounded-xl border"
+                initialSelectedId="7"
+                initialExpandedItems={[
+                  "1",
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10",
+                  "11",
+                  "12",
+                  "13",
+                  "14",
+                  "15",
+                ]}
+              >
+                <InteractiveGridPattern
+                  className={cn(
+                    "mask-[radial-gradient(400px_circle_at_center,white,transparent)]",
+                    "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"
+                  )}
+                />
+                <Folder element="Frameworks" value="1">
+                  <Folder value="2" element="backend-api">
+                    <File value="3" fileIcon={<SiPhp />}>
+                      <p>laravel.php</p>
+                    </File>
+                    <File value="4" fileIcon={<SiNestjs />}>
+                      <p>nest.js</p>
+                    </File>
+                  </Folder>
+                  <Folder value="5" element="web-frontend">
+                    <File value="6" fileIcon={<SiReact />}>
+                      <p>react.js</p>
+                    </File>
+                    <File value="7" fileIcon={<SiTailwindcss />}>
+                      <p>tailwind.css</p>
+                    </File>
+                    <File value="8" fileIcon={<SiNextdotjs />}>
+                      <p>next.js</p>
+                    </File>
+                    {/* <File value="9" fileIcon={<SiNuxtdotjs />}>
+                    <p>nuxt.js</p>
+                  </File> */}
+                  </Folder>
+                  <Folder value="10" element="cross-platform">
+                    <Folder value="11" element="mobile">
+                      <File value="12" fileIcon={<SiReact />}>
+                        <p>react-native.js</p>
+                      </File>
+                      <File value="13" fileIcon={<SiFlutter />}>
+                        <p>flutter.dart</p>
+                      </File>
+                    </Folder>
+                    <Folder value="14" element="desktop">
+                      <File value="15" fileIcon={<SiElectron />}>
+                        <p>electron.js</p>
+                      </File>
+                    </Folder>
+                  </Folder>
+                </Folder>
+              </Tree>
+            </div>
+          <div className="bg-amber-100 rounded-2xl">
+            <Terminal>
+              <TypingAnimation>$ Languages</TypingAnimation>
+              <AnimatedSpan className="text-blue-500">
+                ✔ PHP [Laravel full-stack development]
+              </AnimatedSpan>
+              <AnimatedSpan className="text-blue-500">
+                ✔ Javascript [NodeJS full-stack development]
+              </AnimatedSpan>
+              <AnimatedSpan className="text-blue-500">
+                ✔ Typescript [Typesafe full-stack development]
+              </AnimatedSpan>
+              <AnimatedSpan className="text-blue-500">
+                ✔ Python [Learning DSA and scripting]
+              </AnimatedSpan>
+              <AnimatedSpan className="text-blue-500">
+                ✔ SQL [DBMS, queries, and optimization]
+              </AnimatedSpan>
+            </Terminal>
+          </div>
+          <div className="grid grid-cols-2 gap-3 rounded-2xl">
+            <div className="w-full rounded-md border flex justify-start items-center gap-3 px-3">
+              <SiGit className="w-6 h-6 text-orange-500" />
+              <div>Git</div>
+            </div>
+            <div className="w-full rounded-md border flex justify-start items-center gap-2 p-3">
+              <FcLinux className="w-6 h-6" />
+              <div>Linux</div>
+            </div>
+            <div className="w-full rounded-md border flex justify-start items-center gap-2 p-3">
+              <SiDocker className="w-6 h-6 text-blue-500" />
+              <div>Docker</div>
+            </div>
+            <div className="w-full rounded-md border flex justify-start items-center gap-2 p-3">
+              <SiFigma className="w-6 h-6" />
+              <div>Figma</div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
